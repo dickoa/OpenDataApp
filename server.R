be4324f780ae7771a1024e79251b3ef5ca275057
Dicko Ahmadou <dicko.ahmadou@gmail.com>
2013-02-26 23:43:55 +0000

    Updating App

--- server.R
+++ server.R
@@ -7,43 +7,50 @@ require(RColorBrewer)
 require(ggplot2)
 require(WDI)
 
-        baseurl <- "http://www.pepam.gouv.sn/acces.php?rubr=serv&idreg=%s"
-        nregion <- 11
-        regionName <- c("Dakar", "Ziguinchor", "Diourbel", "Saint-Louis", "Tambacounda", "Kaolack", "Thiès", "Louga", "Fatick", "Kolda", "Matam")
-        urls <- sprintf(baseurl, formatC(seq_len(nregion), digits = 1, flag = "0"))
+setwd("~/ShinyApps/OpenDataApp")
+
+baseurl <- "http://www.pepam.gouv.sn/acces.php?rubr=serv&idreg=%s"
+nregion <- 11
+regionName <- c("Dakar", "Ziguinchor", "Diourbel", "Saint-Louis", "Tambacounda", "Kaolack", "Thiès", "Louga", "Fatick", "Kolda", "Matam")
+urls <- sprintf(baseurl, formatC(seq_len(nregion), digits = 1, flag = "0"))
 
 ### example avec Dakar
-        webpage <- getURLContent(urls[1])
-        tables <- readHTMLTable(webpage)
+webpage <- getURLContent(urls[1])
+tables <- readHTMLTable(webpage)
 
 ### avec les autres voyons voir
-        webpages <- lapply(urls, getURLContent)
-        names(webpages) <- regionName
-        tables <- lapply(webpages, readHTMLTable)
+webpages <- lapply(urls, getURLContent)
+names(webpages) <- regionName
+tables <- lapply(webpages, readHTMLTable)
 
-        data <- sapply(tables, function(x) cbind(as.numeric(gsub(" ", "",as.character(x[[1]]$V2[1]))),
-                                                 as.numeric(as.character(x[[2]]$V2[1])))
-                       )
+data <- sapply(tables, function(x) cbind(as.numeric(gsub(" ", "",as.character(x[[1]]$V2[1]))),
+                                         as.numeric(as.character(x[[2]]$V2[1])))
+               )
 
-        data <- data.frame(t(data))
-        data$region <- regionName
-        names(data) <- c("PopRur", "AccesEau", "region")
+data <- data.frame(t(data))
+data$region <- regionName
+names(data) <- c("PopRur", "AccesEau", "region")
 
 #### download map just once
-        if( ! file.exists("SEN_adm1.shp") ) {
-            mapurl <- "http://gadm.org/data/shp/SEN_adm.zip"
-            download.file(mapurl, destfile = "SEN_adm")
-            unzip("SEN_adm")
-        }
+if( ! file.exists("SEN_adm1.shp") ) {
+    mapurl <- "http://gadm.org/data/shp/SEN_adm.zip"
+    download.file(mapurl, destfile = "SEN_adm")
+    unzip("SEN_adm")
+}
 
-        senreg <- readOGR(dsn = ".", layer = "SEN_adm1")
-        names(senreg@data)[5] <- "region"
-        datafin <- merge(senreg@data, data)
-        senreg@data <- datafin
-        col_eau <- brewer.pal(9,"Blues")
-        col_eau <- colorRampPalette(col_eau, space = "Lab")
-        col_pop <- brewer.pal(9, "YlOrRd")
-        col_pop <- colorRampPalette(col_pop, space = "Lab")
+torem <- file.path(".", c(grep("[0|2|3]", list.files("."), value = TRUE)))
+file.remove(torem)
+
+
+senreg <- readOGR(dsn = ".", layer = "SEN_adm1", encoding = "latin1")
+
+names(senreg@data)[5] <- "region"
+datafin <- merge(senreg@data, data)
+senreg@data <- datafin
+col_eau <- brewer.pal(9,"Blues")
+col_eau <- colorRampPalette(col_eau, space = "Lab")
+col_pop <- brewer.pal(9, "YlOrRd")
+col_pop <- colorRampPalette(col_pop, space = "Lab")
 
 
 
--- ui.R
+++ ui.R
@@ -1,24 +1,23 @@
 require(shiny)
 
-# Define UI for miles per gallon application
+### Define UI for my applications
 shinyUI(pageWithSidebar(
 
   # Application title
   headerPanel("Senegal Open Data visualizer"),
   ## h5("Open data hackathon Senegal made by Ahmadou DICKO"),
 
-  # Sidebar with controls to select the variable to plot against mpg
-                                        # and to specify whether outliers should be included
+### using Side bar
   sidebarPanel(
     selectInput("indicators", "Choisit un indicateur:",
                 choices = c("Population Rurale", "Accès à l'eau", "PIB", "Education et genre")),
-    numericInput("debut", "data de départ", 1960),
+    numericInput("debut", "date de départ", 1960),
     numericInput("fin", "date de fin", 2012),
     numericInput("nobs", "Nombre d'observation", 11),
     helpText(HTML("<br></br>Code source sur <a href = \"https://github.com/dickoa/OpenDataApp\">Github</a>"))
   ),
 
-  # Show the caption and plot of the requested variable against mpg
+
   mainPanel(
     tabsetPanel(
     tabPanel("Graphique", plotOutput("graph")),
